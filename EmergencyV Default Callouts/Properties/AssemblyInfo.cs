da5374a29a4b4248dcf86a35a5a012958fb524bc
alexguirre <alexguirre.aa@gmail.com>
2016-12-04 15:54:55 +0100

    Rename callouts project

---
+++ EmergencyV Default Callouts/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("EmergencyV Default Callouts")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("alexguirre")]
+[assembly: AssemblyProduct("EmergencyV Default Callouts")]
+[assembly: AssemblyCopyright("Copyright ©  2016 alexguirre")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("2f5c1d6b-099f-494c-8076-ea79cd6801c5")]
+[assembly: AssemblyVersion("0.0.1.*")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
